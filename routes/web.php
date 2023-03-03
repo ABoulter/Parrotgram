fbafcdae7ff457ed3ee60d2f726dc1c0f0381472
Exo <paink37@hotmail.com>
2023-03-03 19:52:31 +0000

    Blog post page and database table created and running

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,11 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
-
-
-route::post('/register', [UserController::class, 'register']);
-
-route::post('/login', [UserController::class, 'login']);
-
-route::post('/logout', [UserController::class, 'logout']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
