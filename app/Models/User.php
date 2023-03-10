5c261483321004b58dbf00766e0c300c6046ee89
Exo <paink37@hotmail.com>
2023-03-10 15:40:15 +0000

    Completed Homepage feed functionality adjusted post links

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function feedPosts()
+    {
+        return $this->hasManyThrough(Post::class, Follow::class, 'user_id', 'user_id', 'id', 'followedusers');
+    }
     public function followers()
     {
         return $this->hasMany(Follow::class, 'followeduser');
--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -19,8 +19,10 @@
         </div>
 
         <p class="text-muted small mb-4">
-            <a href="#"><img class="avatar-tiny" src="{{ auth()->user()->avatar }}" /></a>
-            Posted by <a href="#">{{ $post->user->username }}</a> on {{ $post->created_at->format('n/j/y') }}
+            <a href="/profile/{{ $post->user->username }}"><img class="avatar-tiny"
+                    src="{{ auth()->user()->avatar }}" /></a>
+            Posted by <a href="/profile/{{ $post->user->username }}">{{ $post->user->username }}</a> on
+            {{ $post->created_at->format('n/j/y') }}
         </p>
 
         <div class="body-content">
